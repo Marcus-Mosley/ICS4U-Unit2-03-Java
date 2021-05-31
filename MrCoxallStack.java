bc745335610be922d3d589dbac7e0ed917785756
Ubuntu <ubuntu@ip-172-31-94-214.ec2.internal>
2021-05-31 14:59:42 +0000

    Added in StackForm.java and MrCoxallStack.java

---
+++ MrCoxallStack.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+/**
+* This class creates an integer stack.
+*
+* @author  Marcus A. Mosley
+* @version 1.0
+* @since   2021-05-28
+*/
+public class MrCoxallStack {
+  public ArrayList<Integer> stackAsArray = new ArrayList<>();
+  
+  /**
+  * The Push method pushes an integer to the ArrayList.
+  */
+  public void push(int pushNumber) {
+    stackAsArray.add(pushNumber);
+  }
+  
+  /**
+  * The Pop method pops an integer from top of  the ArrayList.
+  */
+  public int pop() {
+    int tmp = stackAsArray.get(stackAsArray.size() - 1);
+    stackAsArray.remove(stackAsArray.size() - 1);
+    return tmp;
+  }
+  
+  /**
+  * The showStack method outputs the ArrayList.
+  */
+  public final void showStack() {
+    System.out.print("\nStack: ");
+    for (int count = 0; count < stackAsArray.size(); count++) {
+      System.out.print(stackAsArray.get(count) + ", ");
+    }
+    System.out.print("\n");
+  }
+}
